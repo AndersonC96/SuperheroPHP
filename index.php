f42afbd2ca0117684947e4e5a856626b4dbfe00b
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2023-07-13 12:08:08 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,30 +8,26 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#">Superhero API</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="characters.php">Ver Personagens</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="random_character.php">Personagem Aleatório</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="search.php">Pesquisar Personagens</a>
-                </li>
-            </ul>
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="characters.php">Personagens</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="random_character.php">Personagem Aleatório</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="search.php">Pesquisar</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="container">
+            <h1>Bem-vindo à Superhero API!</h1>
+            <p>Seu site para explorar heróis e personagens de quadrinhos!</p>
         </div>
-    </nav>
-
-    <div class="container">
-        <h1>Bem-vindo à Superhero API!</h1>
-        <p>Seu site para explorar heróis e personagens de quadrinhos!</p>
-    </div>
-
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    </body>
 </html>
